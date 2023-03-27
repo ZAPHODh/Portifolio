6db440a7077fbb4565848f5b23a1c5aed7db4701
ZAPHODh <luispaulo.ni@gmail.com>
2023-03-27 07:29:33 -0300

    removed clrf to rf

--- src/templates/HomePage/index.tsx
+++ src/templates/HomePage/index.tsx
@@ -4,33 +4,33 @@ import { Projects } from '@/components/Projects';
 import { Tecnologies } from '@/components/Tecnologies';
 import * as Styled from './styles';
 export type HomePageProps = {
-    title?: string;
+  title?: string;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const HomePage = ({ title }: HomePageProps) => {
-    return (
-        <Styled.Wrapper>
-            <Menu />
-            <Perfil
-                buttons={[
-                    {
-                        title: 'Sobre...',
-                        description:
-                            'Tenho 26 anos e sou desenvolvedor a 2. Amo o que faço',
-                    },
-                    {
-                        title: 'Formação',
-                        description: 'Sistema da informação - incompleto',
-                    },
-                    {
-                        title: 'Cursos',
-                        description: 'Em construção',
-                    },
-                ]}
-            />
-            <Tecnologies />
-            <Projects />
-        </Styled.Wrapper>
-    );
+  return (
+    <Styled.Wrapper>
+      <Menu />
+      <Perfil
+        buttons={[
+          {
+            title: 'Sobre...',
+            description:
+              'Tenho 26 anos e sou desenvolvedor a 2. Amo o que faço',
+          },
+          {
+            title: 'Formação',
+            description: 'Sistema da informação - incompleto',
+          },
+          {
+            title: 'Cursos',
+            description: 'Em construção',
+          },
+        ]}
+      />
+      <Tecnologies />
+      <Projects />
+    </Styled.Wrapper>
+  );
 };
