1023471e1fb19eebad3efa0132e824c57d17e49d
ZAPHODh <luispaulo.ni@gmail.com>
2023-03-27 19:24:06 -0300

    prevent game page for not logged user

--- src/pages/games/cobrinha.tsx
+++ src/pages/games/cobrinha.tsx
@@ -1,10 +1,20 @@
 import { SnakeGame } from '@/components/SnakeGame';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 // export type SnakeProps = {
 //   title?: string;
 // };
 
 const Snake = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  useEffect(() => {
+    if (!session) {
+      router.push('/');
+    }
+  }, []);
   return <SnakeGame></SnakeGame>;
 };
 export default Snake;
