2d7635d4d9ef88a76c1a153475899b2c7bf255a7
ZAPHODh <luispaulo.ni@gmail.com>
2023-04-05 11:10:14 -0300

    added centered for large width and flex-start to smartphones

--- src/components/GuessedWordRound/styles.tsx
+++ src/components/GuessedWordRound/styles.tsx
@@ -4,10 +4,15 @@ import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.div`
     ${() => css`
+    width:100%;
+    height:100vh;
     display:flex;
     align-items:center;
     justify-content:center;
     flex-direction:column;
+    @media (max-width:728px){
+        justify-content:flex-start;
+    }
     `}
 `;
 
