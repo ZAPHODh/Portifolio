fa764435098c65972c061b902e23fbf3a9c174b8
ZAPHODh <luispaulo.ni@gmail.com>
2023-06-02 00:30:03 -0300

    back

--- src/templates/HomePage/styles.tsx
+++ src/templates/HomePage/styles.tsx
@@ -3,7 +3,6 @@ import styled, { css } from 'styled-components';
 export const Wrapper = styled.div`
     ${() => css`
         color: #8b8b8b;
-        min-height: 100vh;
         overflow: hidden;
         width: 100%;
         display: flex;
