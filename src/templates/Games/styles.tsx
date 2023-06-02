6191e338f836ed8da28cfb0993fd76ca713e53db
ZAPHODh <luispaulo.ni@gmail.com>
2023-06-01 23:37:54 -0300

    fixing bugs -6

--- src/templates/Games/styles.tsx
+++ src/templates/Games/styles.tsx
@@ -13,7 +13,6 @@ export const Wrapper = styled.div`
     color: white;
     @media (max-width:728px) {
         height: 100%;
-
         }
     >${BannerContainer}{
         height: 300px;
@@ -23,15 +22,13 @@ export const Wrapper = styled.div`
 
 export const GameContent = styled.section`
     ${() => css`
-        height: 800px;
         display: flex;
         align-items: center;
         justify-content: space-between;
-        flex-grow: 1;
         flex: 1;
         @media (max-width:728px) {
             flex-direction: column;
-
+            height: 100%;
         }
     `}
 `;
